3deaf24bc25d863bc523d53aace2865045c483fc
Sunny <p147896325p@gmail.com>
2024-03-26 00:00:42 +0800

    Update  code
    1. create_downloaded_stock_price_db.py: 用來儲存已經下載過的資料的DB。
    2. get_stock_price_data.py: 新增把下載的資料儲存到DB功能。每次抓取時，會先去header確認有沒有下載過資料，若下載過直接從DB抓取，若沒下載過則去交易所下載並塞入DB後，再從DB抓取。

---
+++ create_downloaded_stock_price_db.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+db_file_name = "stock_data.db"
+conn = sqlite3.connect(db_file_name)
+
+
+def create_stock_price_table():
+    # 建立股價資料表
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS stock_daily_price (
+        sid TEXT,
+        month TEXT,
+        date TIMESTAMP,
+        capacity INTEGER,
+        turnover INTEGER,
+        last_close REAL,
+        open REAL,
+        high REAL,
+        low REAL,
+        close REAL,
+        change REAL,
+        "transaction" INTEGER,
+        updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (sid, date)
+    )
+    """)
+
+    conn.commit()
+
+
+def create_stock_header_table():
+    # 建立股票標頭資料表，確認每個月的股票資料是否已經抓取
+    conn.execute("""
+    CREATE TABLE IF NOT EXISTS stock_header (
+        sid TEXT ,
+        month TEXT,
+        updated_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (sid, month)
+    )
+    """)
+    conn.commit()
+
+
+if __name__ == '__main__':
+    create_stock_price_table()
+    create_stock_header_table()
--- get_stock_price_data.py
+++ get_stock_price_data.py
@@ -1,9 +1,21 @@
+from collections import namedtuple
 from datetime import datetime, timedelta
 from typing import List, Union
 
 import numpy as np
 import pandas as pd
 from twstock import Stock
+
+from create_downloaded_stock_price_db import conn
+
+
+def year_month(year, month):
+    return ''.join([str(year), str(month).zfill(2)])
+
+
+DATATUPLE2 = namedtuple('Data',
+                        ['sid', 'month', 'date', 'capacity', 'turnover', 'last_close', 'open', 'high', 'low', 'close',
+                         'change', 'transaction'])
 
 
 class MyStock(Stock):
@@ -16,6 +28,7 @@ class MyStock(Stock):
     date: datetime.datetime格式之時間，例如datetime.datetime(2017, 6, 12, 0, 0)。
     capacity: 總成交股數(單位: 股)。
     turnover: 總成交金額(單位: 新台幣 / 元)。
+    last_close: 昨日收盤價。
     open: 開盤價。
     high: 盤中最高價
     low:盤中最低價。
@@ -34,15 +47,58 @@ class MyStock(Stock):
         self.raw_data = []
         self.data = []
         for year, month in self._month_year_iter(from_month, from_year, to_month, to_year):
-            # 先從DB確認有沒有抓過該股票該月的資料，若有抓過則不再抓取，若沒有則抓取
-            new_fetch_data = self.fetcher.fetch(year, month, self.sid)
-            self.raw_data.append(new_fetch_data)
-            self.data.extend(self.raw_data[-1]['data'])
+            is_this_month = year == to_year and month == to_month
+            year_month_str = year_month(year, month)
+            not_in_db = not self.check_stock_data_in_db(year_month_str)
+            # 先從DB確認有沒有抓過該股票該月的資料，若沒有抓過或是當前月份，則抓取資料塞入DB
+            if is_this_month or not_in_db:
+                new_fetch_data = self.fetcher.fetch(year, month, self.sid)
+                new_fetch_data = new_fetch_data['data']
+                # 存入DB header，若key存在要更新日期
+                conn.execute(
+                    f"INSERT OR REPLACE INTO stock_header VALUES ('{self.sid}', '{year_month_str}', CURRENT_TIMESTAMP)")
+                conn.commit()
+                # 存入DB stock_daily_price
+                for data in new_fetch_data:
+                    conn.execute(
+                        f"INSERT OR REPLACE INTO stock_daily_price VALUES ('{self.sid}', '{year_month_str}', "
+                        f"'{data.date}', {data.capacity}, {data.turnover}, {data.close - data.change}, {data.open}, "
+                        f"{data.high}, {data.low}, {data.close}, {data.change}, {data.transaction}, CURRENT_TIMESTAMP)")
+                    conn.commit()
+            # 從DB取出資料
+            res = conn.execute(
+                f"SELECT * FROM stock_daily_price WHERE sid = '{self.sid}' AND month = '{year_month_str}'")
+            self.purify_data(res.fetchall())
+
         return self.data
 
-    def get_target_date_n_daily_average_price(self, target_date: datetime, n_daily_average: int):
+    def purify_data(self, db_data: List[tuple]):
+        # 轉成datatuple
+        self.data.extend([self.to_datatuple(data) for data in db_data])
+
+    def to_datatuple(self, sub_db_data: tuple):
+        tmp_dict = {
+            'sid': sub_db_data[0],
+            'month': sub_db_data[1],
+            'date': datetime.strptime(sub_db_data[2], '%Y-%m-%d %H:%M:%S'),
+            'capacity': sub_db_data[3],
+            'turnover': sub_db_data[4],
+            'last_close': sub_db_data[5],
+            'open': sub_db_data[6],
+            'high': sub_db_data[7],
+            'low': sub_db_data[8],
+            'close': sub_db_data[9],
+            'change': sub_db_data[10],
+            'transaction': sub_db_data[11]
+        }
+        return DATATUPLE2(**tmp_dict)
+
+    def get_target_date_n_daily_average_price(self, target_date: datetime, n_daily_average: int, soft=True):
         """
         取得指定日期的N日均價。若當日無資料，則往前找到有資料的日期。
+        :param target_date: 指定日期
+        :param n_daily_average: N日均價
+        :param soft: 是否使用軟性搜尋，若為True，則會往前找到有資料的日期，若為False，則會直接回傳None
         stock = MyStock('2330')
         stock.get_target_date_n_daily_average_price(datetime(year=2024, month=2, day=25), 60)
         """
@@ -52,9 +108,12 @@ class MyStock(Stock):
         self.fetch_from_to(pre_month.year, pre_month.month, target_date.year, target_date.month)
         # 去掉晚於target_date的資料
         self.data = [tmp_data for tmp_data in self.data if tmp_data.date <= target_date]
-        # 取得target_date的N日均價
 
-        return self.moving_average(self.price, n_daily_average)[-1], self.data[-1][0]
+        # 若最後一筆資料日期不等於目標日期，代表當日無資料，若soft為False，則回傳None
+        if self.data[-1].date != target_date and not soft:
+            return None, None
+
+        return self.moving_average(self.price, n_daily_average)[-1], self.data[-1].date
 
     def back_test(self, start_backtest_date: datetime, n_daily_average=5,
                   test_day_list: List = [30, 60, 120, 180, 360], silent=False) -> List[Union[float, None]]:
@@ -106,10 +165,15 @@ class MyStock(Stock):
         # 轉成dataframe
         return pd.DataFrame(self.data)
 
+    def check_stock_data_in_db(self, year_month_str: str) -> bool:  # sid = '2330'; year_month_str = '202301'
+        # 確認該股票該月份是否已經抓取過
+        res = conn.execute(f"SELECT * FROM stock_header WHERE sid = '{self.sid}' AND month = '{year_month_str}'")
+        return res.fetchone() is not None
+
 
 if __name__ == '__main__':
     stock = MyStock('2330')
-    stock.back_test(datetime(year=2024, month=3, day=5), 5)
+    stock.back_test(datetime(year=2023, month=3, day=5), 5)
 
 # stock.a = fetch_from1
 #
