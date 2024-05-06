93b21fdf5e15e83a53d5b79c42165247b73b7422
Sunny <p147896325p@gmail.com>
2024-05-07 00:19:24 +0800

    Update Code
    1.add config.pn. Every file uses the folder path set in config.py. Include DB、csv、xlsx. The purpose is to set the path as a cloud drive for development on different computers

---
+++ config.py
@@ -0,0 +1,3 @@
+db_path = "C:/Users/User/iCloudDrive/share_data/db/"
+csv_path = "C:/Users/User/iCloudDrive/share_data/csv/"
+xlsx_path = "C:/Users/User/iCloudDrive/share_data/xlsx/"
--- create_downloaded_stock_price_db.py
+++ create_downloaded_stock_price_db.py
@@ -1,6 +1,8 @@
 import sqlite3
 
-db_file_name = "D:\data\stock_data\stock_data.db"
+from config import db_path
+
+db_file_name = db_path + "stock_data.db"
 conn = sqlite3.connect(db_file_name)
 
 
